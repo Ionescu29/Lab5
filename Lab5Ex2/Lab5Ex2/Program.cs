65f1141bcfaa81ba0af1460d9dc653d30e735f80
Ionescu29 <160519352+Ionescu29@users.noreply.github.com>
2024-03-25 18:44:03 +0200

    Add files via upload

---
+++ Lab5Ex2/Lab5Ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Scrieti un program care sa verifice daca un sir de caractere citit de la
+//tastatura este sau nu palindrom
+Console.WriteLine("introduceti sirul");
+string s = char.Parse(Console.ReadLine());
+for (int i = 0; i < s.Lenght; i++)
+{
+    if (s[i] != s[s.lenght - i - 1])
+    {
+        Console.WriteLine("Sirul este palindrom");
+    }
+    else
+    {
+        Console.WriteLine("sirul nu este plaindrom");
+    }
+}
