57ccbe0ad9f2d4a65b13b7355d5c245561958f68
Ionescu29 <160519352+Ionescu29@users.noreply.github.com>
2024-03-25 18:44:43 +0200

    Add files via upload

---
+++ Lab5Ex3/Lab5Ex3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Scrieti un program care va numara toate aparitiile unui caracter intr-un sir
+//de caractere. Atat caracterul cautat cat si sirul de caractere vor fi citite de la
+//tastatura
+Console.WriteLine("introduceti sirul");
+string[] args =char.Parse(Console.ReadLine());
+Console.WriteLine("introduceti caracterul");
+char c = char.Parse(Console.ReadLine());
+for(int i=0; i<s.Lenght;i++)
+{
+   if(string[c]!=0)
+    {
+        return c;
+    }
+}
