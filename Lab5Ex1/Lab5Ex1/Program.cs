c008fb6fc3b23f6d07e437a7f20dd1e477f5ced7
Ionescu29 <160519352+Ionescu29@users.noreply.github.com>
2024-03-25 18:42:46 +0200

    Add files via upload

---
+++ Lab5Ex1/Lab5Ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Scrieti un program care va afisa pozitia unui substring intr-un string, ambele
+//siruri de caractere fiind citite de la tastatura
+Console.WriteLine("introduceti sirul de caractere");
+string s=char.Parse(Console.ReadLine());
+for(int i=0;i<s.Lenght;i++)
+{
+    string message=s.lenght;
+    string part1 = message.substring(i);
+    return i;
+}
