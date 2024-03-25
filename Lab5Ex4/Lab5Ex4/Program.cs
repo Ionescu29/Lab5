8f02199b0bfa96f0d330c578a70d78b6e95a130d
Ionescu29 <160519352+Ionescu29@users.noreply.github.com>
2024-03-25 18:45:47 +0200

    Add files via upload

---
+++ Lab5Ex4/Lab5Ex4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Scrieti un program care va afisa caracterul cu numarul cel mai mare de aparitii dintr-un string citit
+//de la tastatura, ignorand caseing-ul literelor.
+string[] arg = "Ana are mere";
+char c = 'e';
+for (int i=0;i<s.Lenght;i++)
+{
+    if (string[i] == char.ToString(e))
+        return c++;
+
+}
